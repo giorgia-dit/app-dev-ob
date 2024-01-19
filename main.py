2b47f22a69c0af2edce0e5ec186a499e5f68974c
giorgia-dit <giorgia.ditano@gmail.com>
2024-01-19 13:19:42 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, jsonify
+import random
+
+app = Flask(
+    __name__,
+    template_folder="templates",
+    static_folder="static"
+)
+
+nucleotides = ('A', 'C', 'G', 'T')
+stop_codons = ('TAA', 'TAG', 'TGA')
+sequence = []
+
+def random_item(items):
+    return random.choice(items)
+
+def update_triplet(last_item, triplet):
+    triplet.append(last_item)
+    if len(triplet) > 3:
+        triplet.pop(0)
+
+def check_triplet(triplet, stop_patterns):
+    pattern = ''.join(triplet)
+    if pattern in stop_patterns:
+        return True
+    else:
+        return False
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/update', methods=['GET'])
+def update():
+    nucleotide_value = random_item(nucleotides)
+    update_triplet(nucleotide_value, sequence)
+    check_result = check_triplet(sequence, stop_codons)
+    return jsonify({'nucleotide': nucleotide_value, 'triplet': ''.join(sequence), 'check_result': check_result})
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
